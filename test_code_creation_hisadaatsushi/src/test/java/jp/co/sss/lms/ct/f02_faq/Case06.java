88c3a6b6d0530fc2835f2b9460e4aee99eff52bc
a-hisada <a_hisada@braingate.co.jp>
2026-02-20 15:29:25 +0900

    fix:Case06.java カテゴリ検索正常系テストコード追加
    テスト01から06において、それぞれ取得した値や比較対象が、期待値通りかどうか確認するテストを作成し実施した
    取得した値と期待値は以下のとおり
    ・テスト01
    タイトル:ログイン | LMS
    ・テスト02
    タイトル:コース詳細 | LMS
    ユーザー詳細リンク:ようこそ受講生ＡＡ１さん
    ・テスト03
    タイトル:ヘルプ | LMS
    ・テスト04
    タイトル:よくある質問 | LMS
    ・テスト05
    質問の一覧とカテゴリ検索結果の質問は同じではない:true
    ・テスト06
    (質問内容+回答内容)の中に質問内容は含まれている:true
    (質問内容+回答内容)は質問内容と同じである:false
    質問内容は(質問内容+回答内容)より文字の数が少ない:true

--- test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
+++ test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,42 +40,170 @@ public class Case06 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// ログイン画面が表示されているか確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "login_page");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// ログインIDを入力
+		WebElement loginIdElement = webDriver.findElement(By.name("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("StudentAA01");
+
+		// パスワードを入力
+		WebElement passwordElement = webDriver.findElement(By.name("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser123");
+
+		// ログインボタンを押下
+		WebElement loginButtonElement = webDriver.findElement(By.className("btn"));
+		loginButtonElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// コース詳細画面に遷移したことを確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// ログインできているか確認
+		WebElement userNameElement = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']"));
+		assertEquals("ようこそ受講生ＡＡ１さん", userNameElement.getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "course_details_page");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// 機能ボタンを押下し、ドロップダウンを表示
+		WebElement functionButtonElement = webDriver.findElement(By.linkText("機能"));
+		functionButtonElement.click();
+
+		// ヘルプボタンを押下
+		WebElement helpElement = webDriver.findElement(By.linkText("ヘルプ"));
+		helpElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// ヘルプ画面に遷移したことを確認
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "help_page");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+
+		// ヘルプボタンを押下
+		WebElement FAQElement = webDriver.findElement(By.linkText("よくある質問"));
+		FAQElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// ウィンドウが開いているタブの情報を取得
+		Object[] windowHandles = webDriver.getWindowHandles().toArray();
+
+		// よくある質問画面のタブに移動
+		webDriver.switchTo().window((String) windowHandles[1]);
+
+		// よくある質問画面に遷移したことを確認
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "frequently_asked_questions_page");
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 カテゴリ検索で該当カテゴリの検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+
+		// よくある質問をすべて取得
+		List<WebElement> FAQElements = webDriver.findElements(By.className("sorting_1"));
+
+		// カテゴリ検索前のエビデンスを取得
+		scrollTo("400");
+		getEvidence(new Object() {
+		}, "before_category_search");
+
+		// カテゴリ検索
+		scrollTo("0");
+		WebElement categoryLinkElement = webDriver.findElement(By.linkText("【研修関係】"));
+		categoryLinkElement.click();
+
+		// カテゴリ検索した結果をすべて取得
+		List<WebElement> categorySearchResultElements = webDriver.findElements(By.className("sorting_1"));
+
+		// 検索結果が変わっていることを確認
+		assertNotEquals(FAQElements, categorySearchResultElements);
+
+		// エビデンスを取得
+		scrollTo("300");
+		getEvidence(new Object() {
+		}, "after_category_search");
+
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 検索結果の質問をクリックしその回答を表示")
 	void test06() {
-		// TODO ここに追加
+
+		// カテゴリ検索した結果をすべて取得
+		List<WebElement> categorySearchResultElements = webDriver.findElements(By.className("sorting_1"));
+
+		// 検索結果を押下し、回答内容が表示されているか確認
+		for (WebElement categorySearchResultElement : categorySearchResultElements) {
+
+			// 回答内容を表示させる前のテキストを取得
+			String beforeString = categorySearchResultElement.getText();
+
+			// 回答内容を表示
+			scrollBy("200");
+			categorySearchResultElement.click();
+
+			// 回答内容を表示させた後のテキストを取得
+			String afterString = categorySearchResultElement.getText();
+
+			// 回答内容が表示されているか確認
+			assertTrue(afterString.contains(beforeString));
+			assertFalse(beforeString == afterString);
+			assertTrue(beforeString.length() < afterString.length());
+
+		}
+
+		// エビデンスを取得
+		scrollBy("200");
+		getEvidence(new Object() {
+		}, "category_search_details");
+
 	}
 
 }
