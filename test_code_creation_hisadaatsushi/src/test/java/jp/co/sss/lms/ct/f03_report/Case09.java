ab36e43b347969172d878905706067b745495a59
a-hisada <a_hisada@braingate.co.jp>
2026-02-24 17:04:20 +0900

    fix:Case09.java 受講生レポート登録入力チェックテストコード追加
    テスト01から10において、それぞれ取得した値や比較対象が、期待値通りかどうか確認するテストを作成し実施した
    取得した値と期待値は以下のとおり
    ・テスト01
    タイトル:ログイン | LMS
    ・テスト02
    タイトル:コース詳細 | LMS
    ユーザー詳細リンク:ようこそ受講生ＡＡ１さん
    ・テスト03
    タイトル:ユーザー詳細
    ・テスト04
    タイトル:レポート登録 | LMS
    ・テスト05
    タイトル:レポート登録 | LMS
    学習項目のクラスにerrorInputが含まれている:true
    ・テスト06
    タイトル:レポート登録 | LMS
    理解度のクラスにerrorInputが含まれている:true
    ・テスト07
    タイトル:レポート登録 | LMS
    達成度のクラスにerrorInputが含まれている:true
    ・テスト08
    タイトル:レポート登録 | LMS
    達成度のクラスにerrorInputが含まれている:true
    ・テスト09
    タイトル:レポート登録 | LMS
    達成度のクラスにerrorInputが含まれている:true
    所感のクラスにerrorInputが含まれている:true
    ・テスト10
    タイトル:レポート登録 | LMS
    所感のクラスにerrorInputが含まれている:true
    報告内容のクラスにerrorInputが含まれている:true

--- test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
+++ test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * 結合テスト レポート機能
@@ -35,70 +39,307 @@ public class Case09 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// ログイン画面が表示されているか確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "login_page");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// ログインIDを入力
+		WebElement loginIdElement = webDriver.findElement(By.name("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("StudentAA01");
+
+		// パスワードを入力
+		WebElement passwordElement = webDriver.findElement(By.name("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser123");
+
+		// ログインボタンを押下
+		WebElement loginButtonElement = webDriver.findElement(By.className("btn"));
+		loginButtonElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// コース詳細画面に遷移したことを確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// ログインできているか確認
+		WebElement userNameElement = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']"));
+		assertEquals("ようこそ受講生ＡＡ１さん", userNameElement.getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "course_details_page");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// ユーザー詳細画面に遷移
+		WebElement userNameElement = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']"));
+		userNameElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// ユーザー詳細画面に遷移したことを確認
+		assertEquals("ユーザー詳細", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "user_detail_page");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 該当レポートの「修正する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		// 週報が提出済みのセクションを取得し、詳細ボタンを押下
+		WebElement sectionElement = webDriver
+				.findElement(By.xpath("//tr[td[contains(., '2022年10月2日(日)')]]"));
+		WebElement detailElement = sectionElement.findElement(By.xpath(".//input[@value='修正する']"));
+		scrollBy("300");
+		detailElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// レポート登録画面に遷移したことを確認
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "modify_report_page");
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しエラー表示：学習項目が未入力")
 	void test05() {
-		// TODO ここに追加
+
+		// 学習項目を未入力に変更
+		WebElement learningItemsElement = webDriver.findElement(By.id("intFieldName_0"));
+		learningItemsElement.clear();
+
+		// 提出ボタンを押下
+		WebElement submitBtnElement = webDriver.findElement(By.xpath("//button[text()='提出する']"));
+		scrollBy("400");
+		submitBtnElement.click();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(5);
+
+		// レポート登録画面に遷移したことを確認
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// エラーが表示されているか確認
+		learningItemsElement = webDriver.findElement(By.id("intFieldName_0"));
+		assertTrue(learningItemsElement.getAttribute("class").contains("errorInput"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "learning_items_error");
+
+		// 学習項目に値を入力
+		learningItemsElement.sendKeys("ITリテラシー①");
+
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：理解度が未入力")
 	void test06() {
-		// TODO ここに追加
+
+		// 理解度の選択を解除
+		Select comprehensionSelect = new Select(webDriver.findElement(By.id("intFieldValue_0")));
+		comprehensionSelect.selectByIndex(0);
+
+		// 提出ボタンを押下
+		WebElement submitBtnElement = webDriver.findElement(By.xpath("//button[text()='提出する']"));
+		scrollBy("400");
+		submitBtnElement.click();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(5);
+
+		// レポート登録画面に遷移したことを確認
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		WebElement comprehensionElement = webDriver.findElement(By.id("intFieldValue_0"));
+		assertTrue(comprehensionElement.getAttribute("class").contains("errorInput"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "comprehension_error");
+
+		// 理解度を入力
+		comprehensionSelect = new Select(webDriver.findElement(By.id("intFieldValue_0")));
+		comprehensionSelect.selectByIndex(2);
+
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度が数値以外")
 	void test07() {
-		// TODO ここに追加
+
+		// 達成度を入力
+		WebElement achievementLevelElement = webDriver.findElement(By.id("content_0"));
+		achievementLevelElement.clear();
+		achievementLevelElement.sendKeys("テスト");
+
+		// 提出ボタンを押下
+		WebElement submitBtnElement = webDriver.findElement(By.xpath("//button[text()='提出する']"));
+		scrollBy("400");
+		submitBtnElement.click();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(5);
+
+		// レポート登録画面に遷移したことを確認
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// エラーが表示されているか確認
+		achievementLevelElement = webDriver.findElement(By.id("content_0"));
+		assertTrue(achievementLevelElement.getAttribute("class").contains("errorInput"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "achievement_level_error");
+
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度が範囲外")
 	void test08() {
-		// TODO ここに追加
+
+		// 達成度を入力
+		WebElement achievementLevelElement = webDriver.findElement(By.id("content_0"));
+		achievementLevelElement.clear();
+		achievementLevelElement.sendKeys("12");
+
+		// 提出ボタンを押下
+		WebElement submitBtnElement = webDriver.findElement(By.xpath("//button[text()='提出する']"));
+		scrollBy("400");
+		submitBtnElement.click();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(5);
+
+		// レポート登録画面に遷移したことを確認
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// エラーが表示されているか確認
+		achievementLevelElement = webDriver.findElement(By.id("content_0"));
+		assertTrue(achievementLevelElement.getAttribute("class").contains("errorInput"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "achievement_level_error");
+
 	}
 
 	@Test
 	@Order(9)
 	@DisplayName("テスト09 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：目標の達成度・所感が未入力")
 	void test09() {
-		// TODO ここに追加
+
+		// 達成度の値を削除
+		WebElement achievementLevelElement = webDriver.findElement(By.id("content_0"));
+		achievementLevelElement.clear();
+
+		// 所感の値を削除
+		WebElement impressionsElement = webDriver.findElement(By.id("content_1"));
+		impressionsElement.clear();
+
+		// 提出ボタンを押下
+		WebElement submitBtnElement = webDriver.findElement(By.xpath("//button[text()='提出する']"));
+		scrollBy("400");
+		submitBtnElement.click();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(5);
+
+		// レポート登録画面に遷移したことを確認
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// 達成度にエラーが表示されているか確認
+		achievementLevelElement = webDriver.findElement(By.id("content_0"));
+		assertTrue(achievementLevelElement.getAttribute("class").contains("errorInput"));
+
+		// 所感にエラーが表示されているか確認
+		impressionsElement = webDriver.findElement(By.id("content_1"));
+		assertTrue(impressionsElement.getAttribute("class").contains("errorInput"));
+
+		// エビデンスを取得
+		scrollBy("200");
+		getEvidence(new Object() {
+		}, "achievement_level_and_impressions_error");
+
+		// 達成度に値を入力
+		achievementLevelElement.sendKeys("5");
+
 	}
 
 	@Test
 	@Order(10)
 	@DisplayName("テスト10 不適切な内容で修正して「提出する」ボタンを押下しエラー表示：所感・一週間の振り返りが2000文字超")
 	void test10() {
-		// TODO ここに追加
+
+		// 所感に値を入力
+		WebElement impressionsElement = webDriver.findElement(By.id("content_1"));
+		impressionsElement.clear();
+		impressionsElement.sendKeys("テスト".repeat(667));
+
+		// 報告内容に値を入力
+		WebElement reportElement = webDriver.findElement(By.id("content_2"));
+		reportElement.clear();
+		reportElement.sendKeys("テスト".repeat(667));
+
+		// 提出ボタンを押下
+		WebElement submitBtnElement = webDriver.findElement(By.xpath("//button[text()='提出する']"));
+		scrollBy("400");
+		submitBtnElement.click();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(30);
+
+		// レポート登録画面に遷移したことを確認
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// 所感にエラーが表示されているか確認
+		impressionsElement = webDriver.findElement(By.id("content_1"));
+		assertTrue(impressionsElement.getAttribute("class").contains("errorInput"));
+
+		// 報告内容にエラーが表示されているか確認
+		impressionsElement = webDriver.findElement(By.id("content_2"));
+		assertTrue(impressionsElement.getAttribute("class").contains("errorInput"));
+
+		// エビデンスを取得
+		scrollBy("400");
+		getEvidence(new Object() {
+		}, "impressions_and_report_error");
 	}
 
 }
