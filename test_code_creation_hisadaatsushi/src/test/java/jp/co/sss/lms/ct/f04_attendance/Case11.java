f9fc1732181f73d298e57fc8d8bc82ef541e2937
a-hisada <a_hisada@braingate.co.jp>
2026-02-26 09:32:55 +0900

    fix:Case11.java 受講生勤怠直接編集正常系テストコード追加
    テスト01から04において、それぞれ取得した値や比較対象が、期待値通りかどうか確認するテストを作成し実施した
    取得した値と期待値は以下のとおり
    ・テスト01
    タイトル:ログイン | LMS
    ・テスト02
    タイトル:セキュリティ規約 | LMS
    ・テスト03
    タイトル:勤怠情報変更｜LMS
    ・テスト04
    タイトル:勤怠情報変更｜LMS
    ・テスト05
    タイトル:勤怠情報変更｜LMS
    出勤時間:09:00
    退勤時間:18:00

--- test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
+++ test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +41,134 @@ public class Case11 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// ログイン画面が表示されているか確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "login_page");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// ログインIDを入力
+		WebElement loginIdElement = webDriver.findElement(By.name("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("StudentAA01");
+
+		// パスワードを入力
+		WebElement passwordElement = webDriver.findElement(By.name("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser123");
+
+		// ログインボタンを押下
+		WebElement loginButtonElement = webDriver.findElement(By.className("btn"));
+		loginButtonElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// コース詳細画面に遷移したことを確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// ログインできているか確認
+		WebElement userNameElement = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']"));
+		assertEquals("ようこそ受講生ＡＡ１さん", userNameElement.getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "course_details_page");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// 勤怠ボタンを押下
+		WebElement attendanceBtnElement = webDriver.findElement(By.linkText("勤怠"));
+		attendanceBtnElement.click();
+
+		// アラートのボタンを押下
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		// 勤怠情報管理画面に遷移したことを確認
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "attendance_page");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		// 勤怠情報を直接編集するリンクを押下
+		WebElement editLinkElement = webDriver.findElement(By.linkText("勤怠情報を直接編集する"));
+		editLinkElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// 勤怠情報変更画面に遷移したことを確認
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "attendance_edit_page");
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 すべての研修日程の勤怠情報を正しく更新し勤怠管理画面に遷移")
 	void test05() {
-		// TODO ここに追加
+
+		// 定時ボタンを押下
+		List<WebElement> regularTimeElements = webDriver.findElements(By.xpath("//button[text()='定時']"));
+		for (WebElement regularTimeElement : regularTimeElements) {
+			regularTimeElement.click();
+		}
+
+		// 更新ボタンを押下
+		WebElement completeElement = webDriver.findElement(By.name("complete"));
+		scrollBy("300");
+		completeElement.click();
+
+		// アラートのボタンを押下
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// 勤怠情報変更画面に遷移したことを確認
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
+		// 勤怠時間が記録されていることを確認
+		List<WebElement> timeElement = webDriver.findElements(By.className("w80"));
+		assertEquals("09:00", timeElement.get(2).getText());
+		assertEquals("18:00", timeElement.get(3).getText());
+		assertEquals("09:00", timeElement.get(4).getText());
+		assertEquals("18:00", timeElement.get(5).getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "attendance_regular_time");
+
 	}
 
 }
