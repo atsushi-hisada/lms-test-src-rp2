37e45b6d71fc13db63a234d1f3ca1570fa4ae57f
a-hisada <a_hisada@braingate.co.jp>
2026-02-26 09:27:28 +0900

    fix:Case10.java 受講生勤怠登録正常系テストコード追加
    テスト01から05において、それぞれ取得した値や比較対象が、期待値通りかどうか確認するテストを作成し実施した
    取得した値と期待値は以下のとおり
    ・テスト01
    タイトル:ログイン | LMS
    ・テスト02
    タイトル:セキュリティ規約 | LMS
    ・テスト03
    タイトル:勤怠情報変更｜LMS
    ・テスト04
    今日の出勤時間が空文字ではない
    ・テスト05
    今日の退勤時間が空文字ではない

--- test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +41,126 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// ログイン画面が表示されているか確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "login_page");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// ログインIDを入力
+		WebElement loginIdElement = webDriver.findElement(By.name("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("StudentAA01");
+
+		// パスワードを入力
+		WebElement passwordElement = webDriver.findElement(By.name("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser123");
+
+		// ログインボタンを押下
+		WebElement loginButtonElement = webDriver.findElement(By.className("btn"));
+		loginButtonElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// コース詳細画面に遷移したことを確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// ログインできているか確認
+		WebElement userNameElement = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']"));
+		assertEquals("ようこそ受講生ＡＡ１さん", userNameElement.getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "course_details_page");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// 勤怠ボタンを押下
+		WebElement attendanceBtnElement = webDriver.findElement(By.linkText("勤怠"));
+		attendanceBtnElement.click();
+
+		// アラートのボタンを押下
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		// 勤怠情報管理画面に遷移したことを確認
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "attendance_page");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+
+		// 出勤ボタンを押下
+		WebElement punchInBtnElement = webDriver.findElement(By.name("punchIn"));
+		punchInBtnElement.click();
+
+		// アラートのボタンを押下
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(5);
+
+		// 出勤時間が記録されていることを確認
+		List<WebElement> timeElement = webDriver.findElements(By.className("w80"));
+		assertNotEquals("", timeElement.get(4).getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "punch_in");
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+
+		// 退勤ボタンを押下
+		WebElement punchOutBtnElement = webDriver.findElement(By.name("punchOut"));
+		punchOutBtnElement.click();
+
+		// アラートのボタンを押下
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(5);
+
+		// 退勤時間が記録されていることを確認
+		List<WebElement> timeElement = webDriver.findElements(By.className("w80"));
+		assertNotEquals("", timeElement.get(5).getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "punch_out");
+
 	}
 
 }
