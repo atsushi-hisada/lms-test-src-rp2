48c392ef34cc5d0d884e2f33179036ceed7963ea
a-hisada <a_hisada@braingate.co.jp>
2026-02-27 11:42:07 +0900

    fix:Case13.java 受講生試験の実施結果50点テストコード追加
    テスト01から08において、それぞれ取得した値や比較対象が、期待値通りかどうか確認するテストを作成し実施した
    取得した値と期待値は以下のとおり
    ・テスト01
    タイトル:ログイン | LMS
    ・テスト02
    タイトル:コース詳細 | LMS
    ユーザー詳細リンク:ようこそ受講生ＡＡ１さん
    ・テスト03
    タイトル:セクション詳細 | LMS
    ・テスト04
    タイトル:試験【ITリテラシー①】 | LMS
    ・テスト05
    タイトル:ITリテラシー① | LMS
    ・テスト06
    タイトル:ITリテラシー① | LMS
    ・テスト07
    タイトル:ITリテラシー① | LMS
    ・テスト08
    タイトル:試験【ITリテラシー①】 | LMS
    50.0点の試験結果が存在する:true

--- test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f05_exam/Case14.java
+++ test_code_creation_hisadaatsushi/src/test/java/jp/co/sss/lms/ct/f05_exam/Case14.java
@@ -1,8 +1,10 @@
 package jp.co.sss.lms.ct.f05_exam;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +13,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 試験実施機能
@@ -40,56 +45,218 @@ public class Case14 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// ログイン画面が表示されているか確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "login_page");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// ログインIDを入力
+		WebElement loginIdElement = webDriver.findElement(By.name("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("StudentAA01");
+
+		// パスワードを入力
+		WebElement passwordElement = webDriver.findElement(By.name("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser123");
+
+		// ログインボタンを押下
+		WebElement loginButtonElement = webDriver.findElement(By.className("btn"));
+		loginButtonElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// コース詳細画面に遷移したことを確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// ログインできているか確認
+		WebElement userNameElement = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']"));
+		assertEquals("ようこそ受講生ＡＡ１さん", userNameElement.getText());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "course_details_page");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「試験有」の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// 詳細ボタンを押下
+		List<WebElement> detailBtnElements = webDriver.findElements(By.xpath("//input[@value='詳細']"));
+		detailBtnElements.get(1).click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// セクション詳細画面に遷移したことを確認
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "section_detail_page");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「本日の試験」エリアの「詳細」ボタンを押下し試験開始画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		// 詳細ボタンを押下
+		WebElement detailBtnElement = webDriver.findElement(By.xpath("//input[@value='詳細']"));
+		detailBtnElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// 試験画面に遷移したことを確認
+		assertEquals("試験【ITリテラシー①】 | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "test_page");
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「試験を開始する」ボタンを押下し試験問題画面に遷移")
 	void test05() {
-		// TODO ここに追加
+
+		// 試験を開始するボタンを押下
+		WebElement detailBtnElement = webDriver.findElement(By.xpath("//input[@value='試験を開始する']"));
+		detailBtnElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// 試験画面に遷移したことを確認
+		assertEquals("ITリテラシー① | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "test_input_page");
+
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 正答と誤答が半々で「確認画面へ進む」ボタンを押下し試験回答確認画面に遷移")
 	void test06() {
-		// TODO ここに追加
+
+		// 問題を取得
+		List<WebElement> questionElements = webDriver.findElements(By.className("list-group"));
+
+		// 回答を選択
+		questionElements.get(0).findElements(By.tagName("input")).get(2).click();
+		scrollBy("400");
+		questionElements.get(1).findElements(By.tagName("input")).get(2).click();
+		scrollBy("400");
+		questionElements.get(2).findElements(By.tagName("input")).get(2).click();
+		scrollBy("400");
+		questionElements.get(3).findElements(By.tagName("input")).get(3).click();
+		scrollBy("400");
+		questionElements.get(4).findElements(By.tagName("input")).get(1).click();
+		scrollBy("400");
+		questionElements.get(5).findElements(By.tagName("input")).get(1).click();
+		scrollBy("400");
+		questionElements.get(6).findElements(By.tagName("input")).get(2).click();
+		scrollBy("400");
+		questionElements.get(7).findElements(By.tagName("input")).get(3).click();
+		scrollBy("400");
+		questionElements.get(8).findElements(By.tagName("input")).get(2).click();
+		scrollBy("300");
+		questionElements.get(9).findElements(By.tagName("input")).get(1).click();
+		scrollBy("400");
+		questionElements.get(10).findElements(By.tagName("input")).get(1).click();
+		scrollBy("400");
+		questionElements.get(11).findElements(By.tagName("input")).get(1).click();
+
+		// 確認画面ヘ進むボタンを押下
+		WebElement buttonElement = webDriver.findElement(By.xpath("//input[@value='確認画面へ進む']"));
+		scrollBy("1000");
+		buttonElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// 試験入力画面に遷移したことを確認
+		assertEquals("ITリテラシー① | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "test_confirm_page");
+
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 「回答を送信する」ボタンを押下し試験結果画面に遷移")
 	void test07() throws InterruptedException {
-		// TODO ここに追加
+
+		// 回答を送信するボタンを押下
+		WebElement buttonElement = webDriver.findElement(By.xpath("//button[text()='回答を送信する']"));
+		scrollBy("2000");
+		buttonElement.click();
+
+		// アラートのボタンを押下
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// 試験確認画面に遷移したことを確認
+		assertEquals("ITリテラシー① | LMS", webDriver.getTitle());
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		}, "test_result_page");
+
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 「戻る」ボタンを押下し試験開始画面に遷移後当該試験の結果が反映される")
 	void test08() {
-		// TODO ここに追加
+
+		// 戻るボタンを押下
+		WebElement buttonElement = webDriver.findElement(By.xpath("//input[@value='戻る']"));
+		scrollBy("5000");
+		buttonElement.click();
+
+		// ページが遷移するまで待機
+		pageLoadTimeout(5);
+
+		// 試験画面に遷移したことを確認
+		assertEquals("試験【ITリテラシー①】 | LMS", webDriver.getTitle());
+
+		// 試験結果が存在するか確認
+		WebElement dateElement = webDriver.findElement(By.xpath("//td[contains(.,'50.0点')]"));
+		assertTrue(dateElement.getText().contains("50.0点"));
+
+		// エビデンスを取得
+		scrollBy("500");
+		getEvidence(new Object() {
+		}, "test_complete_page");
+
 	}
 
 }
